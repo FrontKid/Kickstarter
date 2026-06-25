2eb9ce25f25ad3e1c5b82aca90fc49eedae2a743
Poltorak Denys <itwfrontend@gmail.com>
2026-06-25 18:34:40 +0300

    add open/close button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function toggleMenu() {
+  const openButton = document.getElementById('headerBurger');
+  const closeButton = document.getElementById('burgerClose');
+  const burgerMenu = document.getElementById('burgerMenu');
+
+  const openMenu = () => {
+    burgerMenu.classList.add('active');
+  };
+
+  const closeMenu = () => {
+    burgerMenu.classList.remove('active');
+  };
+
+  openButton.addEventListener('click', openMenu);
+
+  closeButton.addEventListener('click', closeMenu);
+}
+
+toggleMenu();
